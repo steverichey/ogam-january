d55463c07ae99ee3e41f62e49dbb1a6a887e07d5
Steve Richey <srichey@floatlearning.com>
2015-01-25 09:55:16 -0600

    Initial Commit

---
+++ ogam-january/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ogam-january
+//
+//  Created by Steve Richey on 1/25/15.
+//  Copyright (c) 2015 stvr. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
